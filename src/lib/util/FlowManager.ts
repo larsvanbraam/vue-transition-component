fbeef62481150ee5ef6228fa768a0c1d4d2f2ee6
Lars van Braam <lars@mediamonks.com>
2017-10-17 13:00:40 +0200

    Disable the pointer events on the body when we are switching pages this avoids interruptions

--- src/lib/util/FlowManager.ts
+++ src/lib/util/FlowManager.ts
@@ -2,8 +2,10 @@ import EventDispatcher from 'seng-event';
 import FlowType from '../enum/FlowType';
 import IAbstractPageTransitionComponent from '../interface/IAbstractPageTransitionComponent';
 import { COMPONENT_ID } from '../mixin/AbstractRegistrableComponent';
+import * as bowser from 'bowser';
 import FlowEvent from '../event/FlowEvent';
 import { Promise } from 'es6-promise';
+import { TweenLite } from 'gsap';
 
 export class FlowManager extends EventDispatcher {
 	/**
@@ -23,11 +25,38 @@ export class FlowManager extends EventDispatcher {
 	 */
 	private _path: string;
 	/**
+	 * @property
+	 * @description Div used when pointer events none is not supported
+	 */
+	private _pointerDiv: HTMLElement;
+	/**
 	 * @public
 	 * @type {Promise<void>}
 	 * @description Promise that contains the hijacked state of the flow
 	 */
 	public flowHijacked: Promise<void> = Promise.resolve();
+
+	constructor() {
+		super();
+
+		// Fallback for IE10
+		if (bowser.msie && bowser.version <= 10) {
+			this._pointerDiv = document.createElement('div');
+
+			TweenLite.set(this._pointerDiv, {
+				className: 'vueTransitionComponentPointerDiv',
+				display: 'none',
+				position: 'fixed',
+				left: '0px',
+				top: '0px',
+				width: '100%',
+				height: '100%',
+				zIndex: 99999,
+			});
+
+			document.body.appendChild(this._pointerDiv);
+		}
+	}
 
 	/**
 	 * @public
@@ -60,6 +89,8 @@ export class FlowManager extends EventDispatcher {
 		this._path = null;
 		// Reset the previous component id when the flow is done to allow re-opening of the same page after closing it
 		this._previousComponentId = null;
+		// Enable the pointer events and allow the flow
+		this.enablePointerEvents();
 	}
 
 	/**
@@ -73,41 +104,58 @@ export class FlowManager extends EventDispatcher {
 	 * @returns {void}
 	 */
 	public start(
-		pageInstance: IAbstractPageTransitionComponent,
-		release: (param?: string | boolean) => void,
-		path: string,
-	): void {
-		if (!this._transitionOut) {
-			if (this._previousComponentId === pageInstance[COMPONENT_ID]) {
-				release();
-			} else {
-				this._path = path;
-				this._previousComponentId = pageInstance[COMPONENT_ID];
-				this.dispatchEvent(new FlowEvent(FlowEvent.START));
-				switch (pageInstance.flow) {
-					case FlowType.NORMAL: {
-						this._transitionOut = pageInstance.transitionOut(true);
-						this._transitionOut.then(() => release());
-						break;
-					}
-					case FlowType.CROSS: {
-						this._transitionOut = pageInstance.transitionOut(true);
-						release();
-						break;
-					}
-					default: {
-						throw new Error('[FlowManager] Unknown flow: [' + pageInstance.flow + ']');
-					}
+				pageInstance: IAbstractPageTransitionComponent,
+				release: (param?: string | boolean) => void,
+				path: string): void {
+		this.disablePointerEvents();
+
+		if (this._previousComponentId === pageInstance[COMPONENT_ID]) {
+			release();
+		} else {
+			this._path = path;
+			this._previousComponentId = pageInstance[COMPONENT_ID];
+			this.dispatchEvent(new FlowEvent(FlowEvent.START));
+			switch (pageInstance.flow) {
+				case FlowType.NORMAL: {
+					this._transitionOut = pageInstance.transitionOut(true);
+					this._transitionOut.then(() => release());
+					break;
+				}
+				case FlowType.CROSS: {
+					this._transitionOut = pageInstance.transitionOut(true);
+					release();
+					break;
+				}
+				default: {
+					throw new Error('[FlowManager] Unknown flow: [' + pageInstance.flow + ']');
 				}
 			}
+		}
+	}
+
+	/**
+	 * @private
+	 * @method disablePointerEvents
+	 * @description Disable pointer events during page switches
+	 */
+	private disablePointerEvents(): void {
+		if (bowser.msie && bowser.version < 10) {
+			this._pointerDiv.style.display = 'block';
 		} else {
-			if (this._path !== path || this.transitionOut === null) {
-				// There is already a page transitioning out, so wait for it and then release the flow!
-				this.transitionOut.then(() => release(true));
-			} else {
-				// Already transitioning out the current page so we do nothing!
-				release(true);
-			}
+			document.body.style.pointerEvents = 'none';
+		}
+	}
+
+	/**
+	 * @private
+	 * @method enablePointerEvents
+	 * @description Enable pointer events and allow flow navigation
+	 */
+	private enablePointerEvents(): void {
+		if (bowser.msie && bowser.version < 10) {
+			this._pointerDiv.style.display = 'none';
+		} else {
+			document.body.style.pointerEvents = 'all';
 		}
 	}
 
@@ -118,6 +166,10 @@ export class FlowManager extends EventDispatcher {
 	public dispose(): void {
 		this._transitionOut = null;
 		this._previousComponentId = null;
+		if (this._pointerDiv) {
+			document.body.removeChild(this._pointerDiv);
+			this._pointerDiv = null;
+		}
 
 		super.dispose();
 	}
