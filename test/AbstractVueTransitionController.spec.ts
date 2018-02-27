730924633f7c77d6814e987f32667f29959ed0fd
Lars van Braam <lars@mediamonks.com>
2018-02-27 20:32:28 +0100

    Add tests for the AbstractVueTransitionController

--- test/AbstractVueTransitionController.spec.ts
+++ test/AbstractVueTransitionController.spec.ts
@@ -1,6 +1,46 @@
+import { TransitionDirection } from 'transition-controller';
 import {} from 'mocha';
+import { expect } from 'chai';
+import { getMountedComponent } from './util/App';
 import { getApplication, getTransitionController } from './util/app/App';
+import ChildComponentA from './util/ChildComponentA/ChildComponentA';
+import IAbstractTransitionComponent from 'lib/interface/IAbstractTransitionComponent';
 
 describe('AbstractTransitionControllerSpec', () => {
-  // TODO: Create tests
+  describe('getSubTimelineByComponent', () => {
+
+    it('should get the subtimeline by the component reference', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentA);
+      return component.$_allComponentsReady
+        .then(() => component.transitionController.getSubTimelineByComponent('ChildComponentB'))
+        .then(component => expect(component).to.not.be.undefined)
+    });
+
+    it('should get the subtimeline by the component instance', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentA);
+      return component.$_allComponentsReady
+        .then(() => component.transitionController.getSubTimelineByComponent(component.$refs.ChildComponentB))
+        .then(component => expect(component).to.not.be.undefined)
+    });
+
+    it('should get the subtimeline by the element', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentA);
+      return component.$_allComponentsReady
+        .then(() => component.transitionController.getSubTimelineByComponent(component.$refs.ChildComponentB.$el))
+        .then(component => expect(component).to.not.be.undefined)
+    });
+
+    it('should get the subtimeline by the component reference, in the in direction', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentA);
+      return component.$_allComponentsReady
+        .then(() => component.transitionController.getSubTimelineByComponent('ChildComponentB', TransitionDirection.IN))
+        .then(component => expect(component).to.not.be.undefined)
+    });
+
+    it('should try to get the subtimeline but fail because it does not exist', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentA);
+      return component.$_allComponentsReady
+        .then(() => expect(() => component.transitionController.getSubTimelineByComponent('Foo')).to.throw(Error))
+    });
+  });
 });
