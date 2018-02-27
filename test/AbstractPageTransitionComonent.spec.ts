2ccbee93afdb53dbf5ae865c2e991c29f0ca2375
Lars van Braam <lars@mediamonks.com>
2018-02-27 20:14:14 +0100

    Add tests for the page component Add tests for the transition component

--- test/AbstractPageTransitionComonent.spec.ts
+++ test/AbstractPageTransitionComonent.spec.ts
@@ -1,6 +1,15 @@
-import { getMountedComponent } from './util/app/App';
+import { expect } from 'chai';
+import { getMountedComponent } from './util/App';
 import {} from 'mocha';
+import IAbstractPageTransitionComponent from '../src/lib/interface/IAbstractPageTransitionComponent';
+import PageComponentA from './util/PageComponentA/PageComponentA';
 
 describe('AbstractPageTransitionComponent', () => {
-  // TODO: Create tests
+  describe('hijackTransitionIn', () => {
+    it('should hijack the transition in until it\'s done', () => {
+      const component = <IAbstractPageTransitionComponent>getMountedComponent(PageComponentA);
+      return component.hijackTransitionIn(resolve => setTimeout(resolve, 1))
+        .then(() => expect(component.$_transitionInHijack).to.not.be.undefined)
+    });
+  });
 });
--- test/AbstractTransitionComponent.spec.ts
+++ test/AbstractTransitionComponent.spec.ts
@@ -1,6 +1,35 @@
 import {} from 'mocha';
-import { getMountedComponent } from './util/app/App';
+import { expect } from 'chai';
+import { getMountedComponent } from './util/App';
+import IAbstractTransitionComponent from 'lib/interface/IAbstractTransitionComponent';
+import ChildComponentB from './util/ChildComponentB/ChildComponentB';
 
 describe('AbstractTransitionComponentSpec', () => {
-  // TODO: Create tests
+  describe('transitionIn', () => {
+    it('should transition in the component', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentB);
+      return component.$_allComponentsReady
+        .then(() => component.transitionIn())
+        .then(() => expect(component.transitionController.isHidden).to.be.false)
+    });
+  });
+
+  describe('transitionOut', () => {
+    it('should transition out the component', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentB);
+      return component.$_allComponentsReady
+        .then(() => component.transitionIn())
+        .then(() => component.transitionOut())
+        .then(() => expect(component.transitionController.isHidden).to.be.true)
+    });
+  });
+
+  describe('$destroy', () => {
+    it('should trigger the destroy method', () => {
+      const component = <IAbstractTransitionComponent>getMountedComponent(ChildComponentB);
+      return component.$_allComponentsReady
+        .then(() => component.$destroy())
+        .then(() => expect(component._isDestroyed).to.be.true)
+    });
+  });
 });
